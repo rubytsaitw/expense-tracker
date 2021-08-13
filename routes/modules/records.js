3e8ffdccb5484b235f2af0898361955af57df3b4
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-13 23:15:27 +0800

    feat: add delete route

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -39,4 +39,14 @@ router.put('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Delete
+router.delete('/:id', (req, res) => {
+  const _id = req.params.id
+  return Record.findOne({ _id })
+    .then((record) => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
 module.exports = router 
