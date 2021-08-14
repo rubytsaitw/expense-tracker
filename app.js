352ebed8b1fc59abf35cad067865e37f29479855
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-14 17:47:25 +0800

    fix: require express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const app = express()
 const PORT = 3000
 const bodyParser = require('body-parser')
